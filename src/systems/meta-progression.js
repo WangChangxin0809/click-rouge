458b958a6f4e4fb2b1ffbbb4f5076f0fb14ec1d0
ChangxinWang <wangchangxin0809@gmail.com>
2026-06-11 17:10:43 +0800

    chore: add test gold (5000) and starter skill for testing

--- src/systems/meta-progression.js
+++ src/systems/meta-progression.js
@@ -69,7 +69,7 @@ const STAT_AMOUNTS = {
 
 const DEFAULT_META = {
     version: 1,
-    permanentGold: 0,
+    permanentGold: 5000,
     unlockedLevels: [1],
     completedLevels: [],
     statUpgrades: {
@@ -80,7 +80,7 @@ const DEFAULT_META = {
         goldMult: 0,
         atkSpeed: 0,
     },
-    ownedSkills: {},
+    ownedSkills: { thunder_strike: 1 },
     ownedFollowers: {},
     ownedEquip: {},
     totalRuns: 0,
